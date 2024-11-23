086e1c292c4c21312010b825e7d13a723b160285
maxdlr <contact@maxdlr.com>
2024-11-23 21:08:37 +0100

    update back packages, add google-java-format & prettier-java

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MddApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MddApiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(MddApiApplication.class, args);
+  }
 }
