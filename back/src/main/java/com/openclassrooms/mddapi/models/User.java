3b1da4f7e82cb7e15725d281ea744566c1dbc7d6
maxdlr <contact@maxdlr.com>
2024-11-26 21:20:13 +0100

    lib: add jakarta validation constraints

---
+++ back/src/main/java/com/openclassrooms/mddapi/models/User.java
@@ -0,0 +1,52 @@
+package com.openclassrooms.mddapi.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(
+    name = "USERS",
+    uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+@Data
+@Accessors(chain = true)
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(of = {"id"})
+@Builder
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NonNull
+  @Size(max = 50)
+  @Email
+  private String email;
+
+  @NonNull
+  @Size(max = 20)
+  @Column(name = "username")
+  private String username;
+
+  @NonNull
+  @Size(max = 120)
+  private String password;
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+}
