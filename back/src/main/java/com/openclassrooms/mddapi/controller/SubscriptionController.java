5690f4d8adcef473b7bca61bc1b72a365786e14e
maxdlr <contact@maxdlr.com>
2024-12-19 21:29:19 +0100

    update session

--- back/src/main/java/com/openclassrooms/mddapi/controller/SubscriptionController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/SubscriptionController.java
@@ -1,8 +1,11 @@
 package com.openclassrooms.mddapi.controller;
 
+import com.openclassrooms.mddapi.models.User;
 import com.openclassrooms.mddapi.payload.request.SubscriptionRequest;
-import com.openclassrooms.mddapi.payload.response.MessageResponse;
+import com.openclassrooms.mddapi.security.jwt.JwtUtils;
 import com.openclassrooms.mddapi.service.SubscriptionService;
+import com.openclassrooms.mddapi.service.UserService;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,22 +16,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/subscriptions")
 public class SubscriptionController {
   private final SubscriptionService subscriptionService;
+  private final UserService userService;
 
-  public SubscriptionController(SubscriptionService subscriptionService) {
+  public SubscriptionController(SubscriptionService subscriptionService, UserService userService) {
     this.subscriptionService = subscriptionService;
+    this.userService = userService;
   }
 
   @PostMapping("/subscribe")
-  public ResponseEntity<MessageResponse> subscribe(
+  public ResponseEntity<List<Long>> subscribe(
       @RequestBody SubscriptionRequest subscriptionRequest) {
     subscriptionService.subscribeUserToTheme(subscriptionRequest);
-    return ResponseEntity.ok().build();
+    return ResponseEntity.ok().body(getCurrentUserThemeIds(subscriptionRequest));
   }
 
   @PostMapping("/unsubscribe")
-  public ResponseEntity<MessageResponse> unsubscribe(
+  public ResponseEntity<List<Long>> unsubscribe(
       @RequestBody SubscriptionRequest subscriptionRequest) {
     subscriptionService.unsubscribeUserFromTheme(subscriptionRequest);
-    return ResponseEntity.ok().build();
+    return ResponseEntity.ok().body(getCurrentUserThemeIds(subscriptionRequest));
+  }
+
+  private List<Long> getCurrentUserThemeIds(SubscriptionRequest subscriptionRequest) {
+    User user = userService.findUser(subscriptionRequest.getUserId());
+    return subscriptionService.findAllSubscriptionThemeIdByUser(user);
   }
 }
